82fcba5b0cf305e44644b0902c7bdbf1b712aee9
dotzero <mail@dotzero.ru>
2016-06-08 09:58:39 +0300

    Initial commit

---
+++ src/GuzzleAdapter.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Habrahabr\Api\HttpAdapter;
+
+use Habrahabr\Api\Exception\NetworkException;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class GuzzleAdapter
+ *
+ * Habrahabr Api HTTP адаптер использующий Guzzle, PHP HTTP client как транспорт
+ *
+ * @package Habrahabr\Api\HttpAdapter
+ * @version 0.1.0
+ * @author dotzero <mail@dotzero.ru>
+ * @link http://www.dotzero.ru/
+ * @link https://habrahabr.ru/
+ * @link https://github.com/dotzero/habrahabr_guzzle-php
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class GuzzleAdapter extends BaseAdapter implements HttpAdapterInterface
+{
+    /**
+     * @var bool Строгая проверка SSL сертификата
+     */
+    protected $strictSSL = true;
+
+    /**
+     * @var array Дополнительные параметры передаваемые в \GuzzleHttp\Client
+     */
+    protected $guzzleConfig = [];
+
+    /**
+     * GuzzleAdapter constructor
+     *
+     * @param array $config Дополнительные параметры передаваемые в \GuzzleHttp\Client
+     */
+    public function __construct(array $config = [])
+    {
+        if ($config) {
+            $this->guzzleConfig = $config;
+        }
+    }
+
+    /**
+     * Выполнить HTTP GET запрос и вернуть тело ответа
+     *
+     * @param string $url URL суффикс запрашиваемого ресурса
+     * @return array
+     * @throws NetworkException
+     */
+    public function get($url)
+    {
+        $url = $this->createUrl($url);
+
+        $response = $this->getGuzzleClient()->get($url);
+
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * Выполнить HTTP POST запрос и вернуть тело ответа
+     *
+     * @param string $url URL суффикс запрашиваемого ресурса
+     * @param array $params Параметры, передаваемые в теле запроса
+     * @return array
+     * @throws NetworkException
+     */
+    public function post($url, array $params = [])
+    {
+        $url = $this->createUrl($url);
+
+        $response = $this->getGuzzleClient()->post($url, [
+            'form_params' => $params
+        ]);
+
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * Выполнить HTTP PUT запрос и вернуть тело ответа
+     *
+     * @param string $url URL суффикс запрашиваемого ресурса
+     * @param array $params Параметры, передаваемые в теле запроса
+     * @return array
+     * @throws NetworkException
+     */
+    public function put($url, array $params = [])
+    {
+        $url = $this->createUrl($url);
+
+        $response = $this->getGuzzleClient()->put($url, [
+            'form_params' => $params
+        ]);
+
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * Выполнить HTTP DELETE запрос и вернуть тело ответа
+     *
+     * @param string $url URL суффикс запрашиваемого ресурса
+     * @param array $params Параметры, передаваемые в теле запроса
+     * @return array
+     * @throws NetworkException
+     */
+    public function delete($url)
+    {
+        $url = $this->createUrl($url);
+
+        $response = $this->getGuzzleClient()->delete($url);
+
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * Устанавливает или убирает строгую проверку SSL сертификата
+     *
+     * @param bool $flag Флаг строгой проверки SSL сертификата
+     * @return $this
+     */
+    public function setStrictSSL($flag = true)
+    {
+        $this->strictSSL = $flag;
+
+        return $this;
+    }
+
+    /**
+     * Возвращает параметры передаваемые в \GuzzleHttp\Client
+     *
+     * @return array
+     */
+    public function getGuzzleConfig()
+    {
+        $config = array_merge([
+            'headers' => [],
+            'http_errors' => false,
+            'verify' => $this->strictSSL,
+            'timeout' => $this->getConnectionTimeout(),
+        ], $this->guzzleConfig);
+
+        if ($this->client !== null && $this->token !== null) {
+            $config['headers']['client'] = $this->client;
+            $config['headers']['token'] = $this->token;
+        } elseif ($this->apikey !== null) {
+            $config['headers']['apikey'] = $this->apikey;
+        }
+
+        return $config;
+    }
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    private function getGuzzleClient()
+    {
+        return new Client($this->getGuzzleConfig());
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return array
+     * @throws NetworkException
+     */
+    private function parseResponse(ResponseInterface $response)
+    {
+        $header = Psr7\parse_header($response->getHeaderLine('content-type'));
+
+        if (!isset($header[0][0]) || $header[0][0] !== 'application/json') {
+            throw new NetworkException(
+                $response->getReasonPhrase(),
+                $response->getStatusCode()
+            );
+        }
+
+        $result = json_decode($response->getBody(), true);
+
+        return $result ? $result : [];
+    }
+}
---
+++ tests/GuzzleAdapterTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Habrahabr\Tests\Api;
+
+use Habrahabr\Api\HttpAdapter\GuzzleAdapter;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+
+
+class GuzzleAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var MockHandler
+     */
+    protected $mock;
+
+    /**
+     * @var GuzzleAdapter
+     */
+    protected $adapter;
+
+    protected function setUp()
+    {
+        $this->mock = new MockHandler([
+            new Response(200,
+                ['Content-Type' => 'application/json'],
+                json_encode(['data' => []])
+            ),
+        ]);
+
+        $this->adapter = new GuzzleAdapter(['handler' => $this->mock]);
+        $this->adapter->setEndpoint('https://habrahabr.ru/');
+    }
+
+    public function testGet()
+    {
+        $res = $this->adapter->get('/foobar?page=1');
+
+        $this->assertArrayHasKey('data', $res);
+        $this->assertInternalType('array', $res['data']);
+
+        $req = $this->mock->getLastRequest();
+
+        $this->assertEquals('GET', $req->getMethod());
+        $this->assertEquals($this->adapter->getEndpoint() . '/foobar?page=1', (string)$req->getUri());
+    }
+
+    public function testPost()
+    {
+        $res = $this->adapter->post('/foobar', [
+            'foo' => 'bar bam',
+            'baz' => ['boo' => 'qux']
+        ]);
+
+        $this->assertArrayHasKey('data', $res);
+        $this->assertInternalType('array', $res['data']);
+
+        $req = $this->mock->getLastRequest();
+
+        $this->assertEquals('POST', $req->getMethod());
+        $this->assertEquals($this->adapter->getEndpoint() . '/foobar', (string)$req->getUri());
+
+        $this->assertEquals(
+            'application/x-www-form-urlencoded',
+            $req->getHeaderLine('Content-Type')
+        );
+
+        $this->assertEquals(
+            'foo=bar+bam&baz%5Bboo%5D=qux',
+            (string)$req->getBody()
+        );
+    }
+
+    public function testPut()
+    {
+        $res = $this->adapter->put('/foobar', [
+            'foo' => 'bar bam',
+            'baz' => ['boo' => 'qux']
+        ]);
+
+        $this->assertArrayHasKey('data', $res);
+        $this->assertInternalType('array', $res['data']);
+
+        $req = $this->mock->getLastRequest();
+
+        $this->assertEquals('PUT', $req->getMethod());
+        $this->assertEquals($this->adapter->getEndpoint() . '/foobar', (string)$req->getUri());
+
+        $this->assertEquals(
+            'application/x-www-form-urlencoded',
+            $req->getHeaderLine('Content-Type')
+        );
+
+        $this->assertEquals(
+            'foo=bar+bam&baz%5Bboo%5D=qux',
+            (string)$req->getBody()
+        );
+    }
+
+    public function testDelete()
+    {
+        $res = $this->adapter->delete('/foobar?page=1');
+
+        $this->assertArrayHasKey('data', $res);
+        $this->assertInternalType('array', $res['data']);
+
+        $req = $this->mock->getLastRequest();
+
+        $this->assertEquals('DELETE', $req->getMethod());
+        $this->assertEquals($this->adapter->getEndpoint() . '/foobar?page=1', (string)$req->getUri());
+    }
+
+    public function testSetStrictSSL()
+    {
+        $this->assertAttributeEquals(true, 'strictSSL', $this->adapter);
+        $this->adapter->setStrictSSL(false);
+        $this->assertAttributeEquals(false, 'strictSSL', $this->adapter);
+    }
+
+    public function testGetGuzzleConfig()
+    {
+        $this->adapter = new GuzzleAdapter([
+            'handler' => $this->mock,
+            'http_errors' => true,
+            'headers' => [
+                'X-Foo' => 'Bar',
+            ]
+        ]);
+        $this->adapter->setStrictSSL(false);
+        $this->adapter->setConnectionTimeout(100);
+        $this->adapter->setEndpoint('https://foobar.com');
+        $this->adapter->setApikey('foobar');
+
+        $config = $this->adapter->getGuzzleConfig();
+
+        $this->assertInstanceOf('GuzzleHttp\Handler\MockHandler', $config['handler']);
+        $this->assertTrue($config['http_errors']);
+        $this->assertFalse($config['verify']);
+        $this->assertEquals(100, $config['timeout']);
+        $this->assertEquals('Bar', $config['headers']['X-Foo']);
+        $this->assertEquals('foobar', $config['headers']['apikey']);
+
+        $this->adapter->setClient('foo.bar');
+        $this->adapter->setToken('foobar');
+
+        $config = $this->adapter->getGuzzleConfig();
+
+        $this->assertEquals('foo.bar', $config['headers']['client']);
+        $this->assertEquals('foobar', $config['headers']['token']);
+    }
+}
