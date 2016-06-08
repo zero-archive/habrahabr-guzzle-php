8bcaf4d22fd7c7ee06716d10530831531fef83b2
dotzero <mail@dotzero.ru>
2016-06-08 10:41:42 +0300

    Typo

--- src/GuzzleAdapter.php
+++ src/GuzzleAdapter.php
@@ -17,7 +17,7 @@ use Psr\Http\Message\ResponseInterface;
  * @author dotzero <mail@dotzero.ru>
  * @link http://www.dotzero.ru/
  * @link https://habrahabr.ru/
- * @link https://github.com/dotzero/habrahabr_guzzle-php
+ * @link https://github.com/dotzero/habrahabr-guzzle-php
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
